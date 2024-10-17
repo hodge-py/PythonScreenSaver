5100b737fb86ab903145be4d85c07c539a633fe3
hodge-py <105604814+hodge-py@users.noreply.github.com>
2024-10-17 15:20:03 -0600

    c

--- main.py
+++ main.py
@@ -2,7 +2,22 @@ import numpy as np
 from mayavi.mlab import *
 from mayavi import mlab
 
-norm = np.random.normal(size=(20,20))
-surf(norm,colormap='gist_earth')
+class PyGraphicsGen:
 
-mlab.show()
+    def __init__(self):
+        pass
+
+
+    def TwoDArrIm(self,background=(.155,.25,.125)):
+        mlab.figure(1,bgcolor=(background))
+        norm = np.random.normal(size=(20,20))
+        imshow(norm,colormap='gist_earth')
+
+        mlab.savefig(filename='test.png')
+
+        mlab.show()
+
+
+host = PyGraphicsGen()
+
+host.TwoDArrIm()
