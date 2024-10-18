b40d16bba1014e934b620b5ada3b0c4038f5c76a
hodge-py <105604814+hodge-py@users.noreply.github.com>
2024-10-18 10:22:45 -0600

    c

--- main.py
+++ main.py
@@ -5,13 +5,36 @@ from PIL import Image
 import random
 import PyQt5
 
-class PyGraphicsGen:
+"""
+Main Code
+"""
 
-    def __init__(self):
-        pass
+class PyGraphicsGen:
+    """
+    PyGraphicsGen is the main class to generate wallpapers, animations, etc.
+    """
+
+    def __init__(self,forward = -.5, right = -.5, up = 0, pitch = 0, roll = 0):
+        self.forward = forward
+        self.right = right
+        self.up = up
+        self.pitch = pitch
+        self.roll = roll
 
 
     def array2dLand(self,data,background=(0,0,0),screenshot=True,mode='rgba',anti=True,showfig=True,imgSize=(1920,1080),colorMaps='gist_earth'):
+        """
+        :param data: 2d numpy array
+        :param background: sets color of scene background
+        :param screenshot: if the photo should be saved
+        :param mode: rgba or rgb. rgba will have a transparent background
+        :param anti: antialiasing on or off
+        :param showfig: show mayavi gui
+        :param imgSize: set the pixel size
+        :param colorMaps: color scheme of figure
+        :return:
+        """
+        mlab.move(self.forward, self.right, self.up)
         fig = mlab.figure(1,bgcolor=background,size=imgSize)
         norm = data
         imshow(norm,colormap=colorMaps)
@@ -29,6 +52,17 @@ class PyGraphicsGen:
 
 
     def surf2dArray(self,data,background=(0,0,0),screenshot=True,mode='rgba',anti=True,showfig=True,imgSize=(1920,1080),colorMaps='gist_earth'):
+        """
+        :param data: 2d numpy array
+        :param background: sets color of scene background
+        :param screenshot: if the photo should be saved
+        :param mode: rgba or rgb. rgba will have a transparent background
+        :param anti: antialiasing on or off
+        :param showfig: show mayavi gui
+        :param imgSize: set the pixel size
+        :param colorMaps: color scheme of figure
+        :return:
+        """
         fig = mlab.figure(1, bgcolor=background,size=imgSize)
         norm = data
         surf(norm, colormap=colorMaps)
@@ -45,10 +79,31 @@ class PyGraphicsGen:
             mlab.show()
 
 
-        def array2dLandAnim(self):
-            pass
+    def array2dLandAnim(self):
+        """
+        :param self:
+        :return:
+        """
+        pass
 
-        def surf2dLArray(self):
-            pass
+    def surf2dLArray(self):
+        """
 
+        :param self:
+        :return:
+        """
+        pass
+
+
+    def setMoveCamera(self,forward,right,up):
+        """
+        Moves the camera to the given coordinates
+        :param forward:
+        :param right:
+        :param up:
+        :return:
+        """
+        self.forward = forward
+        self.right = right
+        self.up = up
 
--- tester.py
+++ tester.py
@@ -6,6 +6,8 @@ host2 = PyGraphicsGen()
 data = np.random.normal(size=(20,20))
 
 #data = np.random.random((20,20))
+host2.setMoveCamera(1000,0,0)
+print(host2.forward)
 
 host2.array2dLand(data,mode='rgb')
 
