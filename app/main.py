5aae11266df22841337ff2126854543c124f05ba
Karson Hodge <105604814+hodge-py@users.noreply.github.com>
2024-10-20 06:05:58 -0600

    c

--- app/main.py
+++ app/main.py
@@ -2,9 +2,19 @@ import sys
 from PySide6 import QtCore, QtGui, QtWidgets
 from PySide6.QtUiTools import QUiLoader
 
+def checked():
+    print("hey")
+
 loader = QUiLoader()
 app = QtWidgets.QApplication(sys.argv)
 window = loader.load("PyGraphicsGen.ui", None)
+window.Generate.clicked.connect(checked)
+
+
+
 window.show()
 app.exec()
+
+
+
 
