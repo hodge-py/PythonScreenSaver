70be1bb2453bd6b194ec58bdd662b8dafa42cdfa
Karson Hodge <105604814+hodge-py@users.noreply.github.com>
2024-10-20 06:35:57 -0600

    c

--- app/app.py
+++ app/app.py
@@ -1,9 +1,14 @@
 import sys
 from PySide6 import QtCore, QtGui, QtWidgets
 from PySide6.QtUiTools import QUiLoader
+from main import PyGraphicsGen
+import numpy as np
 
 def checked():
-    print("hey")
+    pyGraphics = PyGraphicsGen()
+    norms = np.random.normal(size=(20,20))
+    pyGraphics.array2dLand(norms)
+
 
 loader = QUiLoader()
 app = QtWidgets.QApplication(sys.argv)
